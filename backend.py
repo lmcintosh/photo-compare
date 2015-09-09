5cbe37df31f8991e6141458c8b8989283f7e73da
Lane McIntosh <lanemcintosh@gmail.com>
2015-09-08 22:01:04 -0700

    added image times

--- backend.py
+++ backend.py
@@ -2,10 +2,13 @@ import os
 import datetime
 import tkFileDialog
 
-root = os.path.expanduser("~/")
+# Ask user for which directory to run on
+root = os.path.expanduser("~/Dropbox/")
 new_path = tkFileDialog.askdirectory(initialdir=root, title='Please select a directory')
-
 print('Analyzing folder %s' %(new_path))
+
+# thresholds for similar images
+time_threshold = datetime.timedelta(0.0, 10.0) # seconds
 
 # allowable image extensions
 img_exts = ['png', 'PNG', 'jpg', 'JPG']
@@ -13,5 +16,12 @@ img_exts = ['png', 'PNG', 'jpg', 'JPG']
 # get all photos in directories and subdirectories of new_path
 img_names = []
 for (dirpath, dirnames, filenames) in os.walk(new_path):
-    img_names.extend([f for f in filenames if f[-3:] in img_exts])
+    img_names.extend([new_path + '/' + f for f in filenames if f[-3:] in img_exts])
+
+# get the time each image was created
+img_times = []
+for img in img_names:
+    img_times.append(datetime.datetime.fromtimestamp(os.path.getctime(img)))
+
+
 
