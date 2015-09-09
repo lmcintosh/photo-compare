c95ca271a28a0a65033f27157769d4f3d6479459
Lane McIntosh <lanemcintosh@gmail.com>
2015-09-08 22:47:46 -0700

    at plotting two photos

--- backend.py
+++ backend.py
@@ -1,6 +1,9 @@
 import os
 import datetime
 import tkFileDialog
+from matplotlib.pyplot import imshow, show
+import numpy as np
+from scipy import misc
 
 # Ask user for which directory to run on
 root = os.path.expanduser("~/Dropbox/")
@@ -18,10 +21,35 @@ img_names = []
 for (dirpath, dirnames, filenames) in os.walk(new_path):
     img_names.extend([new_path + '/' + f for f in filenames if f[-3:] in img_exts])
 
-# get the time each image was created
+# get the time each image was created, and determine group membership
 img_times = []
+group = []
+group_id = 0
 for img in img_names:
     img_times.append(datetime.datetime.fromtimestamp(os.path.getctime(img)))
+    if not group:
+        group.extend([group_id])
+    elif img_times[-1] - img_times[-2] <= time_threshold:
+        group.extend([group_id])
+    else:
+        group_id += 1
+        group.extend([group_id])
 
+# Now do binary choice on each group
+deletes = np.zeros((len(group),))
+for g in set(group):
+    indices = [i for (i,gr) in zip(np.arange(len(group)),group) if gr==g]
+    if len(indices) > 1:
+        choice = np.random.choice(indices, 2)
 
+        ims = []
+        ims.append(misc.imread(img_names[choice[0]]))
+        ims.append(misc.imread(img_names[choice[1]]))
+        
+        h0 = imshow(ims[0])
+        h1 = imshow(ims[1])
+        show()
+
+        if g > 1:
+            break
 
